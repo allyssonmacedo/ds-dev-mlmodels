3a71cfda2ccf0487528477bde22b721a1bdeca7c
Allysson Macedo <allyssonvsmacedo@gmail.com>
2023-08-27 17:12:42 -0300

    updating files

--- telegram-bot/inner_infos.py
+++
@@ -1,5 +0,0 @@
-#Chave de acesso ao bot (quem tiver a chave consegue controlar o bot)
-CHAVE_API = "6308145803:AAHpRTvYVSKxC1eaqHP1aBdPI7O6xbwYK1k"
-
-#chats das pessoas referentes ao broadcast diário
-chat_ids = {'Allysson':'801431606','Lucas':'1166335113', 'Ismael':'2064851703','Bruno':'1691632243'}
